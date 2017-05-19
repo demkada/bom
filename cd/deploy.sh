221802357659a232c800dfba5738ded8998c7dda
kad D <send@kadary.me>
2017-05-20 00:21:37 +0200

    Fix deployment script

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_d300cda23459_key -iv $encrypted_d300cda23459_iv -in  -in cd/codesigning.asc.enc -out cd/signingkey.asc -d
+    openssl aes-256-cbc -K $encrypted_d300cda23459_key -iv $encrypted_d300cda23459_iv -in cd/codesigning.asc.enc -out cd/signingkey.asc -d
     gpg --fast-import cd/signingkey.asc
     mvn deploy -P sign,build-extras --settings cd/settings.xml
 fi
